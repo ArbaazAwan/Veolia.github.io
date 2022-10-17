5dd41b5abc29c1f9218cb957218cd032b6dfd641
ArbaazAwan <48342373+ArbaazAwan@users.noreply.github.com>
2022-10-17 19:12:22 +0500

    added sites screen

---
+++ src/app/sites/sites-table/sites-table.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sites-table',
+  templateUrl: './sites-table.component.html',
+  styleUrls: ['./sites-table.component.scss']
+})
+export class SitesTableComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() sitesArray!:any[];
+
+  ngOnInit(): void {
+  }
+
+}
