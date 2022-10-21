7208d5987f885aaf45b2079a5da9ef98d1073738
Arbaaz Veolia <arbaaz.rehman.ext@veolia.com>
2022-10-21 12:29:26 +0500

    price/year & cleanup

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-import { DummyChartsComponent } from './dummy-charts/dummy-charts.component';
-import { NgApexchartsModule } from "ng-apexcharts";
-
-import * as CanvasJSAngularChart from '../../assets/canvasjs.angular.component';
 import { ChartsModule } from '../charts/charts.module';
-var CanvasJSChart = CanvasJSAngularChart.CanvasJSChart;
-
-
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    DummyChartsComponent,
-    CanvasJSChart,
   ],
   imports: [
     CommonModule,
-    NgApexchartsModule,
     ChartsModule
-
   ],
   exports:[DashboardComponent]
 })
