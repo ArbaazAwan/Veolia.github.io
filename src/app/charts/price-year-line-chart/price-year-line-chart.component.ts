c5def9b83e7d5bbdc26bb9122fc268a0a68e7d7c
mushahidansari <mushahid.ansari@speridian.com>
2022-11-04 17:33:30 +0500

    fixing build issues

--- src/app/charts/price-year-line-chart/price-year-line-chart.component.ts
+++ src/app/charts/price-year-line-chart/price-year-line-chart.component.ts
@@ -3,69 +3,66 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-price-year-line-chart',
   templateUrl: './price-year-line-chart.component.html',
-  styleUrls: ['./price-year-line-chart.component.scss']
+  styleUrls: ['./price-year-line-chart.component.scss'],
 })
 export class PriceYearLineChartComponent implements OnInit {
-  constructor() { }
+  constructor() {}
   data: any;
   basicOptions: any;
 
   ngOnInit(): void {
-    let years :string[]=[];
-    let prices: number[]=[];
+    let years: string[] = [];
+    let prices: number[] = [];
 
-    function getRandomInt(max:number) {
-      return Math.floor(Math.random() * max);
+    function getRandomInt(max: number) {
+      // return Math.floor(Math.random() * max);
     }
 
-    for(let i = 1; i<=50;i++){
-      years.push("Year " + i.toString());
+    for (let i = 1; i <= 50; i++) {
+      years.push('Year ' + i.toString());
       prices.push(getRandomInt(500));
     }
-    this.basicOptions= {
+    this.basicOptions = {
       responsive: true,
-    maintainAspectRatio: false,
+      maintainAspectRatio: false,
       scales: {
         x: {
           ticks: {
-            color: 'white'
-          }
+            color: 'white',
+          },
         },
         y: {
           ticks: {
             color: 'white',
-            beginAtZero:true
-          }
-        }
+            beginAtZero: true,
+          },
+        },
       },
       plugins: {
         legend: {
           labels: {
-            color: 'white'
-          }
-        }
-      }
+            color: 'white',
+          },
+        },
+      },
     };
     this.data = {
-      labels:years ,
-      datasets:
-      [
-          {
-            label: 'Price',
-              data: prices,
-              tension: 0.4,
-              borderWidth: .8,
-              pointRadius: 5,
-              pointBackgroundColor: "rgba(255, 255, 255, .8)",
-              pointBorderColor: "transparent",
-              borderColor: "rgba(255, 255, 255, .8)",
-              backgroundColor: "transparent",
-              fill: true,
-              maxBarThickness: 6
-          },
-      ]
+      labels: years,
+      datasets: [
+        {
+          label: 'Price',
+          data: prices,
+          tension: 0.4,
+          borderWidth: 0.8,
+          pointRadius: 5,
+          pointBackgroundColor: 'rgba(255, 255, 255, .8)',
+          pointBorderColor: 'transparent',
+          borderColor: 'rgba(255, 255, 255, .8)',
+          backgroundColor: 'transparent',
+          fill: true,
+          maxBarThickness: 6,
+        },
+      ],
+    };
   }
-
-  }
-
 }
