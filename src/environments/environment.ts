de9875f3602205d66a1a4893489b880b1e651d9e
Arbaaz Veolia <arbaaz.rehman.ext@veolia.com>
2022-11-19 18:01:39 +0500

    minor fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // baseUrl: 'https://l33bgpdl2b.execute-api.us-east-1.amazonaws.com/Stage/',
-  baseUrl:'http://127.0.0.1:3000/',
+  baseUrl: 'https://l33bgpdl2b.execute-api.us-east-1.amazonaws.com/Stage/',
+  // baseUrl:'http://127.0.0.1:3000/',
 };
