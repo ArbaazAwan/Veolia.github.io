2ff78e291972346efed313a1b4d45fb80cf25bbc
Arbaaz Veolia <arbaaz.rehman.ext@veolia.com>
2022-11-21 12:39:43 +0500

    deleted update form

--- src/app/master/master.module.ts
+++ src/app/master/master.module.ts
@@ -14,7 +14,6 @@ import {MatInputModule} from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MasterTableComponent } from './master-table/master-table.component';
 import { CreateMasterFormComponent } from './create-master-form/create-master-form.component';
-import { UpdateMasterFormComponent } from './update-master-form/update-master-form.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +21,6 @@ import { UpdateMasterFormComponent } from './update-master-form/update-master-fo
     ViewMasterTableComponent,
     MasterTableComponent,
     CreateMasterFormComponent,
-    UpdateMasterFormComponent,
   ],
   imports: [
     CommonModule,
