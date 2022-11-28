697345cfbd41eb7519319654d999d9075f663b9c
mushahidansari <mushahid.ansari@speridian.com>
2022-11-28 18:46:26 +0500

    fixing client list permission issue for user

--- src/app/clients/clients-list/clients-list.component.ts
+++ src/app/clients/clients-list/clients-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/users/user.service';
+import { ClientService } from '../client.service';
 
 @Component({
   selector: 'app-clients-list',
@@ -7,28 +8,40 @@ import { UserService } from 'src/app/users/user.service';
   styleUrls: ['./clients-list.component.scss'],
 })
 export class ClientsListComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private clientService: ClientService
+  ) {}
 
   clients!: any[];
   isLoading: boolean = false;
   panelOpenState: boolean = false;
+  noclients: boolean = false;
 
   ngOnInit(): void {
     this.isLoading = true;
 
     let userEmail = localStorage.getItem('user_email');
-     this.userService.getUserByEmail(userEmail).subscribe(
-      (users:any)=>{
-        let userId:string = users[0].userId;
-        console.log("userId",userId);
-        this.userService.getClientsByUserId(userId).subscribe((res: any) => {
-          this.clients = res.userClients;
+    this.userService.getUserByEmail(userEmail).subscribe((users: any) => {
+      let userId: string = users[0].userId;
+      if (users[0].role.toLowerCase() == 'user') {
+        this.userService.getClientsByUserId(userId).subscribe({
+          next: (response: any) => {
+            console.log(response);
+            this.clients = response.userClients;
+            this.isLoading = false;
+          },
+          error: (error) => {
+            this.noclients = true;
+            this.isLoading = false;
+          },
+        });
+      } else {
+        this.clientService.getClients().subscribe((res: any) => {
+          this.clients = res;
           this.isLoading = false;
         });
       }
-    )
-
-
+    });
   }
-
 }
