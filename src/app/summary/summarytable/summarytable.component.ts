2fd46fb08807ddeb77651f404e88039cde3bae0a
Arbaaz Veolia <arbaaz.rehman.ext@veolia.com>
2022-11-30 13:47:30 +0500

    summary integration fixes

--- src/app/summary/summarytable/summarytable.component.ts
+++ src/app/summary/summarytable/summarytable.component.ts
@@ -16,16 +16,14 @@ export interface Task {
   styleUrls: ['./summarytable.component.scss'],
 })
 export class SummarytableComponent implements OnInit {
-  allComplete: boolean = false;
-  completed: boolean = false;
+
   selectedAssets: any[] = [];
   searchText: string = '';
 
-  @Input() summaryArray!: any[];
   @Input() isLoading: boolean = false;
   summaryData: any[] = [];
-  @Output() deleteSummaryEvent = new EventEmitter();
-  @Output() eidtSummaryEvent = new EventEmitter();
+
+  selection = new SelectionModel(true,[]);
 
   constructor(private summaryService:SummaryService) {}
 
@@ -37,43 +35,7 @@ export class SummarytableComponent implements OnInit {
     )
   }
 
-  updateAllComplete() {
-    this.allComplete =
-      this.summaryArray != null && this.summaryArray.every((t) => t.isChecked);
-  }
-  someComplete(): boolean {
-    if (this.summaryArray == null) {
-      return false;
-    }
-    return (
-      this.summaryArray.filter((t) => t.completed).length > 0 &&
-      !this.allComplete
-    );
-  }
 
-  setAll(completed: boolean) {
-    this.allComplete = completed;
-    if (this.summaryArray == null) {
-      return;
-    }
-    this.summaryArray.forEach((t) => (t.completed = completed));
-  }
-
-  selection = new SelectionModel<any>(true, []);
-
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.summaryArray.length;
-    return numSelected === numRows;
-  }
-
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected()
-      ? this.selection.clear()
-      : this.summaryArray.forEach((row) => this.selection.select(row));
-  }
 
   deleteSummary(id: any) {
     this.summaryService.deleteSummary(id).subscribe(
