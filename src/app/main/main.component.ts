d4600cae13e3ac50e91880474b03661bc6ef9c55
Mohsin Imran <mohsinimran1999@gmail.com>
2022-12-01 12:26:52 +0500

    Side nav appearence management on clientlist

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  role: any = localStorage.getItem('role');
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  isClientListRoute() {
+    return this.router.url === '/clientslist';
   }
-
 }
