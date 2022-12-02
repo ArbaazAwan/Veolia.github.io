5ae2f56cfb6aefc8e2c91e6e5d3f7514221f5d97
mushahidansari <mushahid.ansari@speridian.com>
2022-12-02 19:03:34 +0500

    adding export functionality in unit page

--- src/app/unit/unit.component.ts
+++ src/app/unit/unit.component.ts
@@ -1,77 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MasterService } from '../master/master.service';
+import { ViewMasterTableComponent } from '../master/view-master-table/view-master-table.component';
 
 @Component({
   selector: 'app-unit',
   templateUrl: './unit.component.html',
   styleUrls: ['./unit.component.scss'],
-  })
+})
 export class UnitComponent implements OnInit {
-
+  @ViewChild(ViewMasterTableComponent) child: ViewMasterTableComponent;
   searchText = '';
   title = 'Unit';
-  masters:any = [];
-  filteredMasters:any = [];
+  masters: any = [];
+  filteredMasters: any = [];
   formGroup!: any;
-  siteId:any = localStorage.getItem('siteId');
-  eventEvalTableShow:boolean = true;
-  asset:FormControl = new FormControl("");
-  masterId:any = null;
+  siteId: any = localStorage.getItem('siteId');
+  eventEvalTableShow: boolean = true;
+  asset: FormControl = new FormControl('');
+  masterId: any = null;
 
-  constructor(private masterService:MasterService){}
+  constructor(private masterService: MasterService) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.initForm();
     this.getMastersBySiteId(this.siteId);
   }
 
-  initForm(){
+  initForm() {
     this.asset.valueChanges.subscribe((response: any) => {
       this.filterData(response);
-    })
-  }
-
-  filterData(enteredData: any){
-    this.filteredMasters = this.masters.filter((master:any) => {
-      return master?.newAssetType?.toLowerCase().indexOf(enteredData) > -1
-      || master?.oldAssetType?.toLowerCase().indexOf(enteredData) > -1
-      || master?.masterSize.toLowerCase().indexOf(enteredData) > -1
-      || master?.masterStyle.toLowerCase().indexOf(enteredData) > -1
     });
   }
 
-
-  getMastersBySiteId(siteId:any){
-    this.masterService.getMastersBySiteId(siteId).subscribe(
-      (res:any)=>{
-        if(res.masters)
-          this.masters = res.masters;
-      }
-    )
+  filterData(enteredData: any) {
+    this.filteredMasters = this.masters.filter((master: any) => {
+      return (
+        master?.newAssetType?.toLowerCase().indexOf(enteredData) > -1 ||
+        master?.oldAssetType?.toLowerCase().indexOf(enteredData) > -1 ||
+        master?.masterSize.toLowerCase().indexOf(enteredData) > -1 ||
+        master?.masterStyle.toLowerCase().indexOf(enteredData) > -1
+      );
+    });
   }
 
-  getSelectedMaster(master:any){
+  getMastersBySiteId(siteId: any) {
+    this.masterService.getMastersBySiteId(siteId).subscribe((res: any) => {
+      if (res.masters) this.masters = res.masters;
+    });
+  }
+
+  getSelectedMaster(master: any) {
     this.masterId = master.masterId;
     this.masterService.setMasterId(master.masterId);
   }
 
-  getDisplayText(master:any){
-    if(master)
-    {
-      return master.oldAssetType + " | " + master?.newAssetType
-      + ", " + master?.masterStyle + ", " + master?.masterSize
-    }
-    else{
+  getDisplayText(master: any) {
+    if (master) {
+      return (
+        master.oldAssetType +
+        ' | ' +
+        master?.newAssetType +
+        ', ' +
+        master?.masterStyle +
+        ', ' +
+        master?.masterSize
+      );
+    } else {
       return '';
     }
-
-
   }
 
-  processModel(){
-    // method needed to be implemented
+  processModel() {
+    this.child.exportToExcel('detailsTable');
   }
-
-
 }
