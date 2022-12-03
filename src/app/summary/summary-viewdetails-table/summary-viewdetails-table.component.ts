e61947a6c0c5fea889c17dc321c65d5095f0f5a1
mushahidansari <mushahid.ansari@speridian.com>
2022-12-03 18:26:34 +0500

    fixing calculation issue

--- src/app/summary/summary-viewdetails-table/summary-viewdetails-table.component.ts
+++ src/app/summary/summary-viewdetails-table/summary-viewdetails-table.component.ts
@@ -8,28 +8,28 @@ import { MasterService } from 'src/app/master/master.service';
   styleUrls: ['./summary-viewdetails-table.component.scss'],
 })
 export class SummaryViewdetailsTableComponent implements OnInit {
-  @Input() summaryArray:any;
+  @Input() summaryArray: any;
   assetTableHeaders: string[] = [];
   assetTableNumbers: string[] = [];
   submitted: boolean = false;
   eventsCosts: number[] = [];
-  overhaulCost:number = 0;
-  yearsArray:any = new Array(50);
-  yearsCosts:any = new Array(50);
+  overhaulCost: number = 0;
+  yearsArray: any = new Array(50);
+  yearsCosts: any = new Array(50);
 
-  clientContractYears:number = 0;
-  clientId:any = localStorage.getItem('clientId');
+  clientContractYears: number = 0;
+  clientId: any = localStorage.getItem('clientId');
 
-  constructor(private masterService:MasterService, private clientService:ClientService) {}
-
-
+  constructor(
+    private masterService: MasterService,
+    private clientService: ClientService
+  ) {}
 
   ngOnInit(): void {
-
-    for(let i= 0; i< this.yearsArray.length; i++){
-      let events:number[] = [];
+    for (let i = 0; i < this.yearsArray.length; i++) {
+      let events: number[] = [];
       this.yearsArray[i] = { events };
-      this.yearsCosts[i] = 0;                 //initializing yearly costs with 0
+      this.yearsCosts[i] = 0; //initializing yearly costs with 0
     }
 
     for (let i = 1; i <= 50; i++) {
@@ -44,27 +44,28 @@ export class SummaryViewdetailsTableComponent implements OnInit {
     this.getContractYears();
 
     this.getMaster(3141); //hardcoded value
-
   }
 
-
-  getMaster(masterId:any){
-    this.masterService.getCompleteMasterById(masterId).subscribe(
-      (master:any)=>{
-
+  getMaster(masterId: any) {
+    this.masterService
+      .getCompleteMasterById(masterId)
+      .subscribe((master: any) => {
         let events = master.events;
         let overhaul = master.overhaul;
         let replacementCost = master.master.replacementCost;
         let lifeMonths = master.master.lifeMonths;
         let overhaulLife = Number(master.master.overhaulLife);
-        console.log("master", master);
+        console.log('master', master);
 
-        console.log("lifeMonths",lifeMonths);
-        let lifePerc = 100/100;
-        let replacementCostYear = Math.ceil((Number(lifeMonths)*lifePerc)/12);
-        console.log("replacementcostyear", replacementCostYear);
+        console.log('lifeMonths', lifeMonths);
+        let lifePerc = 100 / 100;
+        let replacementCostYear = Math.ceil(
+          (Number(lifeMonths) * lifePerc) / 12
+        );
+        console.log('replacementcostyear', replacementCostYear);
 
-        for (let i = 0; i < events?.length; i++) {   //calculating events costs and storing them in array
+        for (let i = 0; i < events?.length; i++) {
+          //calculating events costs and storing them in array
           let totalCostM: number = 0;
           events[i].eventMaintenance?.forEach((evM: any) => {
             totalCostM += Number(evM.evCost);
@@ -79,69 +80,56 @@ export class SummaryViewdetailsTableComponent implements OnInit {
           // console.log("total cost for Event "+(i+1), totalCost + totalCostC);
         }
 
-        if(overhaul){  //calculating overhaul cost
-          overhaul.overhaulMaintenance.forEach(
-            (ohM:any)=>{
-              this.overhaulCost +=  Number(ohM.ohCost)
-            });
-          overhaul.overhaulContractors.forEach(
-            (ohC:any)=>{
-              this.overhaulCost += Number(ohC.ohHour)
-            });
+        if (overhaul) {
+          //calculating overhaul cost
+          overhaul.overhaulMaintenance.forEach((ohM: any) => {
+            this.overhaulCost += Number(ohM.ohCost);
+          });
+          overhaul.overhaulContractors.forEach((ohC: any) => {
+            this.overhaulCost += Number(ohC.ohHour);
+          });
         }
 
-        for(let i =0; i<events.length; i++){ //adding occured events in a year to yearsArray
-          for (let m = 0; m < 600; m++)
-          {
-
-            if (m % events[i].evOccurence === 0){
-              this.yearsArray[m/12].events.push(i);
+        for (let i = 0; i < events.length; i++) {
+          //adding occured events in a year to yearsArray
+          for (let m = 1; m <= 600; m++) {
+            if (m % Number(events[i].evOccurence) === 0) {
+              this.yearsArray[m / 12]?.events?.push(i);
             }
-
           }
         }
 
-        for (let m = 0; m < 600; m++){ //adding overhaul cost to the year
-          if(m % overhaulLife == 0){
-            this.yearsCosts[Math.floor(m/12)] += this.overhaulCost;
-            // console.log("overhaul year cost",this.yearsCosts)
+        for (let m = 0; m < 600; m++) {
+          //adding overhaul cost to the year
+          if (m != 0) {
+            if (m % overhaulLife == 0) {
+              this.yearsCosts[Math.floor(m / 12)] += this.overhaulCost;
+              // console.log("overhaul year cost",this.yearsCosts)
+            }
           }
         }
 
+        for (let y = 1; y < 50; y++) {
+          //calculating yearly costs
 
-        for(let y=0; y < 50; y++){ //calculating yearly costs
-
-          this.yearsArray[y].events.forEach((eventIndex:any)=>{
-            this.yearsCosts[y]+= this.eventsCosts[eventIndex];
-          })
-          if (y % replacementCostYear === 0){
-            this.yearsCosts[y]  += Number(replacementCost);
+          this.yearsArray[y].events.forEach((eventIndex: any) => {
+            this.yearsCosts[y] += this.eventsCosts[eventIndex];
+          });
+          if (y % replacementCostYear === 0 || y == 1) {
+            this.yearsCosts[y] += Number(replacementCost);
           }
-          console.log("year"+(y+1)+" cost", this.yearsCosts[y]);
+          console.log('year' + y + ' cost', this.yearsCosts[y]);
         }
-
-
-
-      }
-    )
+      });
   }
 
-  getContractYears(){
-    this.clientService.getClientById(this.clientId).subscribe(
-      (client:any)=>{
-        this.clientContractYears = client[0].contractYears;
-      }
-    )
+  getContractYears() {
+    this.clientService.getClientById(this.clientId).subscribe((client: any) => {
+      this.clientContractYears = client[0].contractYears;
+    });
   }
-
-
-
-
-
-
 }
 
-export class YearEvents{
-  events:number[]
+export class YearEvents {
+  events: number[];
 }
-
