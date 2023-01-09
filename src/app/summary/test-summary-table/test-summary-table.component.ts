5364ecfe0ed44dcdb551e4565eea298963bfe820
Arbaaz Veolia <arbaaz.rehman.ext@veolia.com>
2023-01-09 18:44:25 +0500

    final editable summary tabel

--- src/app/summary/test-summary-table/test-summary-table.component.ts
+++ src/app/summary/test-summary-table/test-summary-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ConsoleLogger } from '@aws-amplify/core';
+import { MasterService } from 'src/app/master/master.service';
 import { SummaryService } from '../summary.service';
 
 @Component({
@@ -11,11 +14,128 @@ export class TestSummaryTableComponent implements OnInit {
   summaryData: any = [];
   siteId: any = localStorage.getItem('siteId');
   clonedSummaries: { [s: string]: any; } = {};
+  filteredMasters: any = [];
+  asset: FormControl = new FormControl(null);
+  masters: any = [];
+  selectedMaster:any;
+  summary:any;
+  editing:boolean = false;
 
-  constructor(private summaryService: SummaryService) { }
+  form:FormGroup = this.fb.group({
+    unit: '',
+    masterId: [{ value: '', disabled: true }],
+    assetType: '',
+    summarySize: '',
+    summaryStyle: '',
+    appDescription: '',
+    dutyApplication: '',
+    quality: '',
+    quantity: '1',
+    summaryload: '100',
+    life: [null, Validators.required],
+    installmentDate: [null, Validators.required],
+  })
+
+  constructor(private summaryService: SummaryService, private masterService:MasterService, private fb:FormBuilder) { }
 
   ngOnInit(): void {
     this.getSummaries();
+    this.asset.valueChanges.subscribe((value: any) => {
+      this.filterData(value);
+    });
+    this.getMasters();
+  }
+
+
+  onAssetChange(master: any) {
+
+    this.summary.masterId = master.masterId;
+    this.summary.unit = this.getUnit(master);
+    this.summary.assetType = master.oldAssetType + ' - ' + master.newAssetType;
+    this.summary.summaryStyle = master.masterStyle;
+    this.summary.summarySize = master.masterSize;
+    this.summary.dutyApplication = master.dutyApplication;
+    this.summary.quality = master.quality;
+
+  }
+
+  onInstallmentChange(summary:any) {
+
+    let x = summary.installmentDate.split('-');
+    let installmentDate = Number(x[0]);
+
+    let currentYear = Number(new Date().getFullYear());
+    let installationYear = Number(installmentDate);
+    let yearsPassed = currentYear - installationYear;
+    let totalYears = 0;
+    if (this.selectedMaster?.lifeMonths) {
+      totalYears = Math.ceil(Number(this.selectedMaster?.lifeMonths) / 12);
+      let lifePerc = Math.round(
+        ((totalYears - yearsPassed) / totalYears) * 100
+      );
+      summary.life = lifePerc;
+    } else {
+      this.masterService.getMasterById(summary.masterId).subscribe((res: any) => {
+        let master = res[0];
+        totalYears = Math.ceil(Number(master?.lifeMonths) / 12);
+        let lifePerc = Math.round(
+          ((totalYears - yearsPassed) / totalYears) * 100
+        );
+        summary.life = lifePerc;
+      });
+    }
+  }
+
+  getUnit(master: any) {
+    if (
+      master.oldAssetType ||
+      master.newAssetType ||
+      master.masterStyle ||
+      master.masterSize ||
+      master.dutyApplication ||
+      master.quality
+    ) {
+      return (
+        master?.oldAssetType +
+        ' - ' +
+        master?.newAssetType +
+        ', ' +
+        master?.masterStyle +
+        ', ' +
+        master?.masterSize +
+        ', ' +
+        master?.dutyApplication +
+        ', ' +
+        master?.quality
+      );
+    } else {
+      return '';
+    }
+  }
+
+  filterData(enteredData: any) {
+    enteredData = enteredData.toString().toLowerCase();
+    this.filteredMasters = this.masters.filter((master: any) => {
+      return (
+        master?.newAssetType?.toLowerCase().indexOf(enteredData) > -1 ||
+        master?.oldAssetType?.toLowerCase().indexOf(enteredData) > -1 ||
+        master?.masterSize?.toLowerCase().indexOf(enteredData) > -1 ||
+        master?.masterStyle?.toLowerCase().indexOf(enteredData) > -1
+      );
+    });
+  }
+
+  getMasters() {
+    this.masterService.getMasters().subscribe(
+      {
+        next: (res: any) => {
+          this.masters = res.masters;
+        },
+        error: (error) => {
+          this.masterService.openSnackBar('No record found in master table', 'close');
+        }
+      }
+    );
   }
 
   getSummaries(){
@@ -31,6 +151,8 @@ export class TestSummaryTableComponent implements OnInit {
 
   onRowEditInit(summary: any) {
     this.clonedSummaries[summary.summaryId] = { ...summary };
+    this.summary = summary;
+    this.editing = true;
   }
 
   onRowEditSave(summary: any) {
@@ -64,6 +186,7 @@ export class TestSummaryTableComponent implements OnInit {
           this.summaryService.openSnackBar('Error occured during update.', 'close')
         }
       });
+      this.editing = false;
   }
 
   onRowDelete(summary:any){
@@ -84,6 +207,7 @@ export class TestSummaryTableComponent implements OnInit {
   onRowEditCancel(summary: any, index: any) {
     this.summaryData[index] = this.clonedSummaries[summary.summaryId];
     delete this.summaryData[summary.summaryId];
+    this.editing = false;
   }
 
 
