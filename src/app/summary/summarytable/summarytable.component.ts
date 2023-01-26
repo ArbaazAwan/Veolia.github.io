b7f066ebf9241c30be4579f7af19f3118e8737b5
Maham-Bukhari <maham.awais@speridian.com>
2023-01-26 16:35:28 +0500

    update

--- src/app/summary/summarytable/summarytable.component.ts
+++ src/app/summary/summarytable/summarytable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ThemePalette } from '@angular/material/core';
 import { MasterService } from 'src/app/master/master.service';
@@ -20,6 +20,7 @@ export interface Task {
 })
 export class SummarytableComponent implements OnInit {
 
+  @ViewChild('hiddenFormBtn') modal: ElementRef;
   summaries: any = [];
   filteredSummaries: any = [];
   siteId: any = localStorage.getItem('siteId');
@@ -32,17 +33,37 @@ export class SummarytableComponent implements OnInit {
   isLoading: boolean = false;
   searchText!: FormControl;
   dateValid: boolean = true;
-  siteStatus:boolean=false;
+  siteStatus: boolean = false;
+  showButton: boolean = false;
+  public showForm = false;
 
-  constructor(private summaryService: SummaryService, private masterService: MasterService,private siteService:SiteService) { }
+
+  constructor(private summaryService: SummaryService, private masterService: MasterService,
+     private siteService: SiteService) { }
 
   ngOnInit(): void {
     this.getSiteStatus();
     this.getSummaries();
+    setTimeout(() => {
+      this.showButton = true;
+    }, 1500);
     this.asset.valueChanges.subscribe((value: any) => {
       this.filterData(value);
     });
     this.getMasters();
+
+  }
+
+  toggleModal() {
+    this.showForm = !this.showForm;
+
+    if (this.showForm) {
+      setTimeout(
+        () => {
+          this.modal.nativeElement.click();
+        }, 2000
+      )
+    }
 
   }
 
@@ -57,7 +78,7 @@ export class SummarytableComponent implements OnInit {
     })
   }
 
-  onAssetChange(master: any,summary:any) {
+  onAssetChange(master: any, summary: any) {
 
     this.selectedMaster = master;
     summary.masterId = master.masterId;
@@ -85,7 +106,7 @@ export class SummarytableComponent implements OnInit {
         lifePerc = 100;
       }
       summary.life = lifePerc;
-      summary.remainingLife = 100 -lifePerc;
+      summary.remainingLife = 100 - lifePerc;
 
       if (lifePerc < 0) {
         this.dateValid = false;
@@ -109,7 +130,7 @@ export class SummarytableComponent implements OnInit {
           lifePerc = 100;
         }
         summary.life = lifePerc;
-        summary.remainingLife = 100 -lifePerc;
+        summary.remainingLife = 100 - lifePerc;
         if (lifePerc < 0) {
           this.dateValid = false;
           this.summaryService.openSnackBar(
@@ -198,7 +219,7 @@ export class SummarytableComponent implements OnInit {
   }
 
   onRowEditInit(summary: any) {
-    let date = new Date (summary.installmentDate);
+    let date = new Date(summary.installmentDate);
     summary.installmentDate = date;
     this.clonedSummaries[summary.summaryId] = { ...summary };
   }
@@ -232,7 +253,7 @@ export class SummarytableComponent implements OnInit {
       siteId: summary.siteId,
       masterId: summary.masterId,
       unit: summary.unit,
-      eqpFunctionalDesc:summary.eqpFunctionalDesc,
+      eqpFunctionalDesc: summary.eqpFunctionalDesc,
       assetType: summary.assetType,
       assetId: summary.assetId,
       importAssetType: summary.importAssetType,
@@ -271,8 +292,8 @@ export class SummarytableComponent implements OnInit {
         next: (_) => {
           delete this.clonedSummaries[summary.summaryId];
           this.summaryService.openSnackBar('Record deleted successfully!', 'close');
-          // this.getSummaries();
-          window.location.reload();
+          this.getSummaries();
+          // window.location.reload();
         },
         error: (_) => {
           this.summaryService.openSnackBar('Error occured during update.', 'close')
