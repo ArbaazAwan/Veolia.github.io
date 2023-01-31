113534bd5d435e92f67cd29ee3a2f296a7e61419
Arbaaz Veolia <arbaaz.rehman.ext@veolia.com>
2023-01-31 22:39:23 +0500

    cleanup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // baseUrl: 'https://6dbjs4k2fk.execute-api.us-east-1.amazonaws.com/Stage/',
+  baseUrl: 'https://6dbjs4k2fk.execute-api.us-east-1.amazonaws.com/Stage/',
 
-  baseUrl: 'http://127.0.0.1:3000/',
+  // baseUrl: 'http://127.0.0.1:3000/',
 };
